57a39f19808c9850b194d31c5cfd6d7941914852
cesarpa <cesar.1023@hotmail.com>
2016-01-29 21:43:24 -0500

    first commit CESAR

---
+++ VirtualGame/Assets/javascript/MovimientoJugador.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoJugador : MonoBehaviour {
+
+    public float moveSpeed = 100.0f;      // Audio clip of the player shouting.
+    public float jumpForce = 1000f;
+
+    private Animator anim;              // Reference to the animator component.
+ 
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("CESAR: Start ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((Vector3.forward)*moveSpeed * Time.deltaTime);
+          
+        if (Input.GetKeyDown("space")) {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            Debug.Log("CESAR: Space Bar");
+        }
+
+     
+
+     
+    }
+}
+
+
---
+++ VirtualGame/Assets/javascript/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("CESAR: Start ");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("CESAR");
+	}
+}
