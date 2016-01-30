1650519e09da7df039f52254293f7f1b2a1db2a1
Johan S Delgado <johanseb0104@hotmail.com>
2016-01-30 18:49:35 -0500

    union escenario con personaje

---
+++ VirtualGame/Assets/Backgrounds/scripts/OffsetScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour {
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+	void Start () {
+        savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+
+    }
+	
+	void Update () {
+        //float x = Mathf.Repeat();
+        Vector2 offset = new Vector2(Time.time * scrollSpeed, 0);
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+    void OnDisable(){
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
---
+++ VirtualGame/Assets/Backgrounds/scripts/movieTexture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movieTexture : MonoBehaviour {
+
+    public MovieTexture mT;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
