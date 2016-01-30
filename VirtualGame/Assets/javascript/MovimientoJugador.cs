492dbaa85ac651c55b9469757793c3119f74525a
cesarpa <cesar.1023@hotmail.com>
2016-01-30 11:05:08 -0500

    subida personaje sin animacion aun

--- VirtualGame/Assets/javascript/MovimientoJugador.cs
+++ VirtualGame/Assets/javascript/MovimientoJugador.cs
@@ -3,32 +3,42 @@ using System.Collections;
 
 public class MovimientoJugador : MonoBehaviour {
 
-    public float moveSpeed = 100.0f;      // Audio clip of the player shouting.
-    public float jumpForce = 1000f;
-
-    private Animator anim;              // Reference to the animator component.
- 
+    public float moveSpeed = 10f;      // Audio clip of the player shouting.
+    public float jumpForce = 100f;
+    public float jumping = 0;
+    private Animator anim;
+    // Reference to the ,animator component.
+    public Rigidbody rigid;
 
     // Use this for initialization
     void Start()
     {
         Debug.Log("CESAR: Start ");
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate((Vector3.forward)*moveSpeed * Time.deltaTime);
-          
-        if (Input.GetKeyDown("space")) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-            Debug.Log("CESAR: Space Bar");
+        if(jumping < 2) {   //SALTA SI NO HA DADO EL DOBLE SALTO
+            if (Input.GetKeyDown("space")) {
+                rigid.AddForce(Vector3.up * jumpForce);
+              //  xrigidbody.AddForce(
+              //  GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+                Debug.Log("CESAR: Space Bar");
+                jumping++;
+            }
+            if(jumping == 2){ //TO DO : LOGICA CUANDO CAE AL SUELO
+                jumping = 0;
+            }
         }
 
-     
 
-     
+
+
     }
+    
 }
 
 
