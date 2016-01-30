aab49e9e0d939c70b032596c2e3777823b4a36af
Juan Felipe Arias <juantrullo@gmail.com>
2016-01-30 10:40:52 -0500

    jfa - Se agrega el movimiento del personaje - Texturas nuevas

---
+++ VirtualGame/Assets/javascript/CamareMain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamareMain : MonoBehaviour {
+
+	public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FollowCharacter ();
+	}
+
+	void FollowCharacter(){
+		//transform.Translate ((character.transform.position.x)*Time.deltaTime,0,-10);
+	}
+}
---
+++ VirtualGame/Assets/javascript/Esfera.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Esfera : MonoBehaviour {
+
+    public float velocity = 10f;
+    private int direcMovimiento;
+	private bool jump;
+	public float forceJump = 20f;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		direcMovimiento = 1;
+		jump = false;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		MoveEsfera();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ValidarTeclas ();
+		JumpKey ();
+	}
+
+	void ValidarTeclas(){
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Debug.Log ("Derecha: "+direcMovimiento);
+			direcMovimiento = 1;
+		} 
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			Debug.Log ("Izquierda: "+direcMovimiento);
+			direcMovimiento = -1;
+		}
+		else {
+			direcMovimiento = 0;
+		}
+		MoveEsfera ();
+	}
+    
+    void MoveEsfera(){
+		
+		transform.Translate((velocity*Time.deltaTime)*direcMovimiento,0,0);
+    }
+
+	void JumpKey(){
+		if(Input.GetButton("Jump")){
+			Debug.Log ("Oprimio el salto");
+			rb.AddForce(Vector3.up*forceJump);
+		}
+	}
+}
